f15015e577a7842f56a17e6058a3049ecb06f89d
jhon <jhon@DESKTOP-9T4EFST>
2022-11-13 21:56:42 -0500

    Agregar archivos de proyecto.

---
+++ capitulo4/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ejercicio4_1
+//ejercicio elavororado por jhon esteban ortiz pascuaza 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("escriba el numero");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escriba el numero");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a % b == 0)
+            {
+                Console.WriteLine(b + " el resultado de divisor  " + a);
+            }
+            else
+            {
+                if (b % a == 0)
+                {
+                    Console.WriteLine(a + " el resultado de divisor " + b);
+                }
+                else
+                {
+                    Console.WriteLine("los numero digitados no son divisores ");
+                }
+
+
+
+
+            }
+        }
+    }
+}
+
+
---
+++ ejercicio4.9/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ejercicio3_1
+//ejercicio elavorado por jhon esteban ortiz pascuaza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("resta o suma ");
+            Console.WriteLine("escriba el primer dato ");
+            double num1, num2;
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("escriba el primer dato");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite 1(suma) o 2(resta) para mirar el resultado");
+            int opcion;
+
+            opcion = int.Parse(Console.ReadLine());
+            if (opcion == 1)
+            {
+                double suma;
+                suma = num1 + num2;
+                Console.WriteLine("el total de la sumatoria es de : " + suma);
+            }
+            if (opcion == 2)
+            {
+                double resta;
+                resta = num1 - num2;
+                Console.WriteLine("el total de la resta es des: " + resta);
+
+            }
+
+        }
+    }
+}
